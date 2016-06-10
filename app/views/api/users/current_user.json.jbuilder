27032f1d8b09811b91e894fea10b70bec1b437a7
App Academy Student <appacademy@Apps-Mac-mini-74.local>
2016-06-10 13:47:23 -0400

    fixes default images

--- app/views/api/users/current_user.json.jbuilder
+++ app/views/api/users/current_user.json.jbuilder
@@ -1,4 +1,4 @@
 json.id @user.id
 json.first_name @user.first_name
 json.last_name @user.last_name
-json.postPicUrl @user.profile_pic.url(:post)
+json.postPicUrl asset_path(@user.profile_pic.url(:post))
---
+++ frontend/components/profile/ProfilePic.jsx
@@ -0,0 +1,74 @@
+var React = require('react'),
+    SessionStore = require('../../stores/session'),
+    UserApiUtil = require('../../util/user_api_util'),
+    UserStore = require('../../stores/user');
+
+var ProfilePic = React.createClass({
+  getInitialState: function () {
+    return ({profilePicUrl: this.props.profilePicUrl});
+  },
+  render: function () {
+    var profileOwnerId = this.props.profileOwnerId,
+        currentUserId = SessionStore.currentUser().id;
+
+    var authorizedToEdit = (profileOwnerId === currentUserId);
+
+    var editProfilePicButton =
+      <div className='empty-edit-profile-pic-button' />;
+
+    if (authorizedToEdit) {
+      editProfilePicButton = (
+        <form>
+          <div className='profile-pic-input-container'>
+            <div className='profile-pic-input-replacement'>
+              <i className="fa fa-camera" aria-hidden="true"></i>
+              <strong>Change cover photo</strong>
+            </div>
+
+            <div className='profile-pic-input-cover' />
+
+            <input type='file'
+              className='profile-pic-input'
+              id='profile-pic-input'
+              onChange={this.updateProfilePicFile}>
+            </input>
+          </div>
+        </form>
+      );
+    }
+
+    if (this.state.profilePicUrl) {
+      profilePicUrl = this.state.profilePicUrl;
+    } else {
+      profilePicUrl = this.props.profilePicUrl;
+    }
+
+    var profilePic = (
+      <div className='profile-pic'>
+        <img src={profilePicUrl} />
+        {editProfilePicButton}
+      </div>
+    );
+
+    return profilePic;
+  },
+  componentWillReceiveProps: function (newProps) {
+    this.forceUpdate();
+  },
+  updateProfilePicFile: function (e) {
+    var profilePicFile = e.currentTarget.files[0];
+    var fileReader = new FileReader();
+    fileReader.onloadend = function() {
+      var formData = new FormData();
+      formData.append('user[profile_pic]', profilePicFile);
+      UserApiUtil.submitProfilePic(formData);
+    };
+
+    if (profilePicFile) {
+      fileReader.readAsDataURL(profilePicFile);
+    }
+  }
+});
+
+
+module.exports = ProfilePic;
