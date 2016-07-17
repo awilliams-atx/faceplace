3b1033ab132a09515d271dc7624bf65cd048f2bc
Andrew Williams <andrew.williams.atx@gmail.com>
2016-07-17 15:50:58 -0400

    ErrorActions #clearErrors takes an errorType, dynamically sets actionType in dispatch based on received errorType

--- frontend/actions/error_actions.js
+++ frontend/actions/error_actions.js
@@ -2,10 +2,16 @@ var Dispatcher = require('../dispatcher/dispatcher'),
     errorConstants = require('../constants/error_constants');
 
 var ErrorActions = {
-  clearErrors: function () {
+  clearErrors: function (errorType) {
+    var actionType;
+    if (errorType === 'login') {
+      actionType = errorConstants.LOGIN_ERRORS_CLEARED;
+    } else if (errorType === 'signUp') {
+      actionType = errorconstants.SIGN_UP_ERRORS_CLEARED;
+    }
     Dispatcher.dispatch({
-      actionType: errorConstants.ERRORS_CLEARED
-    })
+      actionType: actionType
+    });
   },
   setErrors: function (errors, errorType) {
     Dispatcher.dispatch({
