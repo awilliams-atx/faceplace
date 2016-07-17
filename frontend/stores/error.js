042f1b20afcfcbe2ef416dc028a3a1da6ce8df51
Andrew Williams <andrew.williams.atx@gmail.com>
2016-07-17 14:55:40 -0400

    Error store ERRORS_RECEIVED passes payload.errorType into #setErrors

--- frontend/stores/error.js
+++ frontend/stores/error.js
@@ -13,7 +13,7 @@ ErrorStore.__onDispatch = function (payload) {
       ErrorStore.__emitChange();
       break;
     case errorConstants.ERRORS_RECEIVED:
-      this.setErrors(payload.errors);
+      this.setErrors(payload.errors, payload.errorType);
       ErrorStore.__emitChange();
       break;
   }
