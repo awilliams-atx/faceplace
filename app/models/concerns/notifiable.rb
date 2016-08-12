fb73fd92d2dd2463d2af5761e917ecf23b47f5b0
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-12 12:19:43 -0400

    concern/notifiable removes user_id, takes in options hash of notifying_user_id & notified_user_id

--- app/models/concerns/notifiable.rb
+++ app/models/concerns/notifiable.rb
@@ -4,11 +4,12 @@ module Notifiable
     has_many :notifications, as: :notifiable
   end
 
-  def make_notification(user_id)
+  def make_notification(options)
     notif = Notification.new
     notif.notifiable_type = self.class.to_s
     notif.notifiable_id = self.id
-    notif.user_id = user_id
+    notif.notifying_user_id = options[:notifying_user_id]
+    notif.notified_user_id = options[:notified_user_id]
     notif.save!
   end
 end
