9971c74c7f02f6b0ee3fe9aa9266d9367dc529ff
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-19 21:57:26 -0400

    FriendIndexItem#render reflects userId -> user_id change

--- frontend/components/profile/timeline/friends/FriendIndexItem.jsx
+++ frontend/components/profile/timeline/friends/FriendIndexItem.jsx
@@ -6,7 +6,7 @@ var FriendIndexItem = React.createClass({
 
     return (
       <div className='friend-thumb'>
-        <a href={'#/users/' + this.props.friend.userId}>
+        <a href={'#/users/' + this.props.friend.user_id}>
           <img src={friend.thumbUrl}/>
           <div className='friend-thumb-name'>
             {friend.fullName}
