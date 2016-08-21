da43f965c858ca09fdf1d150a0883f81282a36e9
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-21 16:26:32 -0400

    ProfilePic removes unnecessary #componentWillReceiveProps update logic

--- frontend/components/profile/ProfilePic.jsx
+++ frontend/components/profile/ProfilePic.jsx
@@ -3,24 +3,17 @@ var React = require('react'),
     SessionStore = require('../../stores/session');
 
 var ProfilePic = React.createClass({
-  getInitialState: function () {
-    return ({profilePicUrl: this.props.profilePicUrl});
-  },
   render: function () {
     return (
       <div className='profile-pic'>
         <img src={this.props.profilePicUrl} />
         <EditButton authorizedToEdit={this.authorizedToEdit()}
-          updateProfilePicFile={this.updateProfilePicFile}
           updateUtil='submitProfilePic'
           formName='user[profile_pic]'
           photoClassName='profile-pic'
           />
       </div>
     );
-  },
-  componentWillReceiveProps: function (props) {
-    this.setState({profilePicUrl: props.profilePicUrl});
   },
   authorizedToEdit: function () {
     return this.props.profileOwnerId === SessionStore.currentUser().id;
