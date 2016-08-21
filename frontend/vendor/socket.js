8694ad8e2ac4864cb42a478a768e1c12ead6f02b
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-20 22:10:42 -0400

    Reverts Socket into ES5 syntax

--- frontend/vendor/socket.js
+++ frontend/vendor/socket.js
@@ -1,42 +1,36 @@
 var config = require('./config.js'),
     SessionStore = require('../stores/session');
 
-class Socket {
-  constructor (channelName, channelId) {
-    this.config = config.channels[channelName];
-    this.pusher = new Pusher(config.pusherId, { encrypted: true });
-    this.channelId = SessionStore.currentUser().id;
-    this.subscribe(channelId);
-  }
-
-  // Only call #subscribe directly to change subscriptions
-
-  subscribe () {
-    if (this.isSubscribed) {
-      this.unsubscribe();
-    }
-    this.channel = this.pusher.subscribe(this.channelName());
-    this.bindEvents();
-    this.isSubscribed = true;
-  }
-
-  // PRIVATE
-
-  bindEvents () {
-    var events = this.config.events;
-    events.forEach(function (event) {
-      var eventName = Object.keys(event)[0];
-      this.channel.bind(eventName, event[eventName])
-    }.bind(this));
-  }
-
-  channelName () {
-    return this.config.channelRoot + this.channelId;
-  }
-
-  unsubscribe () {
-    this.pusher.unsubscribe(this.channelName());
-  }
+function Socket (channelName, channelId) {
+  this.config = config.channels[channelName];
+  this.pusher = new Pusher(config.pusherId, { encrypted: true });
+  this.channelId = SessionStore.currentUser().id;
+  this.subscribe(channelId);
 }
+
+Socket.prototype.subscribe = function () {
+  if (this.isSubscribed) {
+    this.unsubscribe();
+  }
+  this.channel = this.pusher.subscribe(this.channelName());
+  this.bindEvents();
+  this.isSubscribed = true;
+};
+
+Socket.prototype.bindEvents = function () {
+  var events = this.config.events;
+  events.forEach(function (event) {
+    var eventName = Object.keys(event)[0];
+    this.channel.bind(eventName, event[eventName])
+  }.bind(this));
+};
+
+Socket.prototype.channelName = function () {
+  return this.config.channelRoot + this.channelId;
+};
+
+Socket.prototype.unsubscribe = function () {
+  this.pusher.unsubscribe(this.channelName());
+};
 
 module.exports = Socket;
