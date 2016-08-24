517bfc228012daa0ada00e2da3826e7388ccd6b5
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-24 19:29:41 -0400

    adds after_create :add_watching, after_destroy :remove_watching

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,22 @@
 class Comment < ActiveRecord::Base
   validates :commentable_id, :commentable_type, presence: true
 
-  belongs_to :author, class_name: 'User'
+  after_create :add_watching
+  after_destroy :remove_watching
 
+  belongs_to :author, class_name: 'User'
   belongs_to :commentable, polymorphic: true
+
+  private
+
+  def add_watching
+    Watching.find_or_create_by(watchable_type: 'Post', watchable_id:
+      commentable_id, watcher_id: author_id)
+  end
+
+  def remove_watching
+    watching = Watching.find_by(watchable_type: 'Post', watchable_id:
+      commentable_id, watcher_id: author_id)
+    watching.destroy if watching
+  end
 end
