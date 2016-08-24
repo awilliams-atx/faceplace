c6e971369abef27e31cc1ea34853d9332f407487
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-24 19:24:11 -0400

    adds after_create :add_watching, after_destroy :remove_watching

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -2,6 +2,22 @@ class Tagging < ActiveRecord::Base
   include Notifiable
   validates :tagged_id, :post_id, presence: true
 
+  after_create :add_watching
+  after_destroy :remove_watching
+
   belongs_to :post
   belongs_to :tagged, class_name: 'User', foreign_key: :tagged_id
+
+  private
+
+  def add_watching
+    Watching.create!(watchable_type: 'Post', watchable_id: post_id, watcher_id:
+      tagged_id)
+  end
+
+  def remove_watching
+    watching = Watching.find_by(watchable_type: 'Post', watchable_id: post_id,
+      watcher_id: tagged_id)
+    watching.destroy if watching
+  end
 end
