487ad9dc0872e9d50a44ccbe02fefedc6e105d2e
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-24 19:37:55 -0400

    adds after_create :add_watching, after_destroy :remove_watching

--- app/models/timeline_posting.rb
+++ app/models/timeline_posting.rb
@@ -2,13 +2,23 @@ class TimelinePosting < ActiveRecord::Base
   include Notifiable
   validates :post_id, :profile_owner_id, presence: true
 
+  after_create :add_watching
+  after_destroy :remove_watching
+
+  has_one :author, through: :post, source: :author
   belongs_to :post
+  belongs_to :profile_owner, class_name: 'User', foreign_key: :profile_owner_id
 
-  belongs_to :profile_owner,
-    class_name: 'User',
-    foreign_key: :profile_owner_id
+  private
 
-  has_one :author,
-    through: :post,
-    source: :author
+  def add_watching
+    Watching.create!(watchable_type: 'Post', watchable_id: post_id, watcher_id:
+      profile_owner_id)
+  end
+
+  def remove_watching
+    watching = Watching.find_by(watchable_type: 'Post', watchable_id: post_id,
+      watcher_id: profile_owner_id)
+    watching.destroy if watching
+  end
 end
