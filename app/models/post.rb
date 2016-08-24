9eba96e3d72af86b666b2ea1846dcf46ab6db2ae
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-24 19:34:16 -0400

    adds after_create :add_watching, after_destroy :remove_watching

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,29 @@
 class Post < ActiveRecord::Base
   validates :author_id, presence: true
 
+  after_create :add_watching
+  after_destroy :remove_watching
+
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-
   has_many :comments, as: :commentable
-
   has_many :commenters, through: :comments, class_name: 'User', foreign_key:
     :author_id, source: :author
-
   has_one :profile_owner, through: :timeline_posting, source: :profile_owner
-
-  has_many :taggings, dependent: :destroy
-
   has_many :tagged_friends, through: :taggings, source: :tagged
-
+  has_many :taggings, dependent: :destroy
   has_one :timeline_posting, dependent: :destroy
-
   has_many :watchings, as: :watchable, dependent: :destroy
+
+  private
+
+  def add_watching
+    Watching.create!(watchable_type: 'Post', watchable_id: id, watcher_id:
+      author_id)
+  end
+
+  def remove_watching
+    watching = Watching.find_by(watchable_type: 'Post', watchable_id: id,
+      watcher_id: author_id)
+    watching.destroy if watching
+  end
 end
