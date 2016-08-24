440e1f866a1c38d6f61b6e771482bed6eff46e87
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-24 18:08:53 -0400

    adds #watchers association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,29 +1,20 @@
 class Post < ActiveRecord::Base
   validates :author_id, presence: true
 
-  belongs_to :author,
-    class_name: 'User',
-    foreign_key: :author_id
-
-  has_many :taggings, dependent: :destroy
-
-  has_many :tagged_friends,
-    through: :taggings,
-    source: :tagged
-
-  has_one :timeline_posting
-
-  has_one :profile_owner,
-    through: :timeline_posting,
-    source: :profile_owner
-
-  has_one :timeline_posting, dependent: :destroy
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   has_many :comments, as: :commentable
 
-  has_many :commenters,
-    through: :comments,
-    class_name: 'User',
-    foreign_key: :author_id,
-    source: :author
+  has_many :commenters, through: :comments, class_name: 'User', foreign_key:
+    :author_id, source: :author
+
+  has_one :profile_owner, through: :timeline_posting, source: :profile_owner
+
+  has_many :taggings, dependent: :destroy
+
+  has_many :tagged_friends, through: :taggings, source: :tagged
+
+  has_one :timeline_posting, dependent: :destroy
+
+  has_many :watchers, as: :watchable
 end
