0c85ec0a174368e1d71c90f706c6639cf9e4dcfa
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-24 19:15:02 -0400

    cleaning up

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -2,9 +2,6 @@ class Tagging < ActiveRecord::Base
   include Notifiable
   validates :tagged_id, :post_id, presence: true
 
-  belongs_to :tagged,
-    class_name: 'User',
-    foreign_key: :tagged_id
-
   belongs_to :post
+  belongs_to :tagged, class_name: 'User', foreign_key: :tagged_id
 end
