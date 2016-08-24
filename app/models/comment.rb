5fbd96496349d468fb03587d3e44f68eecc2c383
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-24 18:58:25 -0400

    cleaning up

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,7 @@
 class Comment < ActiveRecord::Base
   validates :commentable_id, :commentable_type, presence: true
 
-  belongs_to :commentable, polymorphic: true
-  has_many :comments, as: :commentable
+  belongs_to :author, class_name: 'User'
 
-  belongs_to :author,
-    class_name: 'User'
+  belongs_to :commentable, polymorphic: true
 end
