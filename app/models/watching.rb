7bc5ff8a612df4b3d2b0489d5cd63f5e5f0732a8
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-24 20:08:12 -0400

    adds associations #watcher, #watchable

--- app/models/watching.rb
+++ app/models/watching.rb
@@ -2,4 +2,6 @@ class Watching < ActiveRecord::Base
   validates :watchable_id, :watchable_type, :watcher_id, presence: true
   validates :watcher_id, uniqueness: { scope: :watchable_id, message: 'Watcher
     already watching watchable' }
+  belongs_to :watcher, class_name: 'User'
+  belongs_to :watchable, polymorphic: true
 end
