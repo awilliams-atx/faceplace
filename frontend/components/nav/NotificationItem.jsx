fac31faa783949a047cb1397d3e0279ae719842a
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-25 19:33:14 -0400

    wraps notifier_name in span.notifier-name

--- frontend/components/nav/NotificationItem.jsx
+++ frontend/components/nav/NotificationItem.jsx
@@ -3,11 +3,14 @@ var React = require('react');
 var NotificationItem = React.createClass({
   render: function () {
     return (
-      <div className={'nav-drop-item group' + this.props.checkedClass}>
+      <div className={'nav-drop-item group ' + this.props.checkedClass}>
         <img src={this.props.notif.profile_pic_url}
           className='nav-drop-profile-pic nav-drop-block' />
         <div className='notification-details nav-drop-block'>
-          {this.props.notif.explanation}
+          <span className='notifier-name'>
+            {this.props.notif.notifier_name}
+          </span>
+          &nbsp;{this.props.notif.explanation}
         </div>
       </div>
     );
