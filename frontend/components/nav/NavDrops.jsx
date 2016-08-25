3813eea822b25ed83739ca77c59c9a0f0499ccae
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-25 10:25:35 -0400

    renders Notifications beside FriendRequests

--- frontend/components/nav/NavDrops.jsx
+++ frontend/components/nav/NavDrops.jsx
@@ -1,11 +1,14 @@
 var React = require('react'),
-    FriendRequests = require('./FriendRequests');
+    FriendRequests = require('./FriendRequests'),
+    Notifications = require('./Notifications');
 
 var NavDrops = React.createClass({
   render: function () {
     return (
       <div id='nav-drops' className='group'>
         <FriendRequests dropToggles={this.props.dropToggles}
+          toggleNavDrop={this.props.toggleNavDrop} />
+        <Notifications dropToggles={this.props.dropToggles}
           toggleNavDrop={this.props.toggleNavDrop} />
       </div>
     );
