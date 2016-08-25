986d2cdc8e88ba0e56cbe6fdc4721213b0ff04a3
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-25 10:38:20 -0400

    explanation logic moved to Notification model

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -2,23 +2,8 @@ class Api::NotificationsController < ApplicationController
   before_action :require_login
 
   def index
-    @notifications =
-      current_user.notifications.includes(:notifier).map do |notif|
-      formatted = {}
-      case notif.notifiable_type
-      when 'Tagging'
-        formatted = {}
-        formatted[:profile_pic] = notif.notifier.profile_pic.url
-        formatted[:message] = "#{notif.notifier.full_name} tagged you in a post."
-        formatted
-      when 'TimelinePosting'
-        formatted = {}
-        formatted[:profile_pic] = notif.notifier.profile_pic.url
-        formatted[:message] = "#{notif.notifier.full_name} posted on your timeline."
-        formatted
-      end
-    end
-    render json: @notifications
+    @notifications = current_user.notifications
+    render 'api/notifications/index'
   end
 
   def destroy
