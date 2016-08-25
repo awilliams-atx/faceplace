442d9f7b9f7d5446dac3e5847c493a661177f569
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-24 20:23:54 -0400

    adds after_create :make_notification

--- app/models/timeline_posting.rb
+++ app/models/timeline_posting.rb
@@ -1,7 +1,7 @@
 class TimelinePosting < ActiveRecord::Base
   validates :post_id, :profile_owner_id, presence: true
 
-  after_create :add_watching
+  after_create :add_watching, :make_notification
   after_destroy :remove_watching
 
   has_one :author, through: :post, source: :author
@@ -13,6 +13,11 @@ class TimelinePosting < ActiveRecord::Base
   def add_watching
     Watching.find_or_create_by!(watchable_type: 'Post', watchable_id: post_id,
       watcher_id: profile_owner_id)
+  end
+
+  def make_notification
+    Notification.create!(notifiable_type: 'Post', notifiable_id: post_id,
+      notifier_id: author.id, notified_id: profile_owner.id)
   end
 
   def remove_watching
