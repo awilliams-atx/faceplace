30621ba2150eadfee5af87f072dbe9ab239f56c6
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-25 11:08:50 -0400

    adds CHECKED_NOTIFICATION_IDS_RECEIVED

--- frontend/stores/notification.js
+++ frontend/stores/notification.js
@@ -4,10 +4,15 @@ var Store = require('flux/utils').Store,
 
 var NotificationStore = new Store(AppDispatcher);
 
+_justCheckedIds = [];
 _notifications = [];
 
 NotificationStore.__onDispatch = function (payload) {
   switch (payload.actionType) {
+  case notificationConstants.CHECKED_NOTIFICATION_IDS_RECEIVED:
+    NotificationStore.markNotificationsChecked(payload.checked_ids);
+    NotificationStore.__emitChange();
+    break;
   case notificationConstants.NOTIFICATIONS_RECEIVED:
     this.setNotifications(payload.notifications);
     NotificationStore.__emitChange();
@@ -16,6 +21,21 @@ NotificationStore.__onDispatch = function (payload) {
 
 NotificationStore.all = function () {
   return _notifications.slice();
+};
+
+NotificationStore.markNotificationsChecked = function (checked_ids) {
+  NotificationStore.setJustCheckedIds(checked_ids);
+  for (var i = 0; i < _notifications.length; i++) {
+    if (checked_ids.indexOf(_notifications[i].id) >= 0) {
+      _notifications[i].checked = true;
+    }
+  }
+};
+
+NotificationStore.setJustCheckedIds = function (checked_ids) {
+  for (var i = 0; i < checked_ids.length; i++) {
+    _justCheckedIds.push(checked_ids[i]);
+  }
 };
 
 NotificationStore.setNotifications = function (notifications) {
