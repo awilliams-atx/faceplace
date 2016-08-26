dce23a0264c930f988bfd7a5df45c707a0f0c8d6
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-26 15:59:23 -0400

    calls this.props.rollUp regardless what page the post to jump to is on

--- frontend/components/nav/NotificationItem.jsx
+++ frontend/components/nav/NotificationItem.jsx
@@ -38,9 +38,8 @@ var NotificationItem = React.createClass({
         pathname: pushPath,
         query: { post_id: this.props.notif.post_id }
       });
-    } else {
-      this.props.rollUp();
     }
+    this.props.rollUp();
   },
   renderDate: function () {
     var currentTime = new Date();
