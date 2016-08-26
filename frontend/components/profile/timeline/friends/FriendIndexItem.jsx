9bb5c9d322140de3b7ada9a896857e18a758b733
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-26 01:28:53 -0400

    reconciles change to browserHistory

--- frontend/components/profile/timeline/friends/FriendIndexItem.jsx
+++ frontend/components/profile/timeline/friends/FriendIndexItem.jsx
@@ -1,22 +1,25 @@
 var React = require('react');
 
 var FriendIndexItem = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
   render: function () {
-    var friend = this.props.friend;
-
     return (
       <div className='friend-thumb'>
-        <a href={'#/users/' + this.props.friend.user_id}>
-          <img src={friend.profile_pic_url}/>
+        <a href={'/users/' + this.props.friend.user_id}
+          onClick={this.pushUserRoute}>
+          <img src={this.props.friend.profile_pic_url}/>
           <div className='friend-thumb-name'>
-            {friend.fullName}
+            {this.props.friend.fullName}
           </div>
         </a>
       </div>
     );
   },
-  componentWillReceiveProps: function () {
-    this.forceUpdate();
+  pushUserRoute: function (e) {
+    e.preventDefault();
+    this.context.router.push('/users/' + this.props.friend.user_id);
   }
 });
 
