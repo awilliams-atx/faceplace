9d3a17ff4873fac607645eddcf8f286282c4702b
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-27 21:58:39 -0400

    cleaning up

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,8 +1,7 @@
 class FriendRequest < ActiveRecord::Base
   validates :maker_id, uniqueness: { scope: :receiver_id }
   validates :maker_id, :receiver_id, presence: true
-  belongs_to :maker,
-    class_name: 'User'
-  belongs_to :receiver,
-    class_name: 'User'
+  
+  belongs_to :maker, class_name: 'User'
+  belongs_to :receiver, class_name: 'User'
 end
