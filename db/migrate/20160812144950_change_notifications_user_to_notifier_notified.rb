4308a0a5ef3d2244252022ffba8eb39c203a4920
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-28 22:11:26 -0400

    fixes corrupt migration

--- db/migrate/20160812144950_change_notifications_user_to_notifier_notified.rb
+++ db/migrate/20160812144950_change_notifications_user_to_notifier_notified.rb
@@ -1,9 +1,6 @@
 class ChangeNotificationsUserToNotifierNotified < ActiveRecord::Migration
   def change
     rename_column :notifications, :user_id, :notified_id
-    add_column :notifications, :  validates :notifiable_id, :notifiable_type,
-      :notified_id, :notifier_id, presence: true
-
-    notifier_id, :integer, null: false
+    add_column :notifications, :notified_id, :integer, null: false
   end
 end
