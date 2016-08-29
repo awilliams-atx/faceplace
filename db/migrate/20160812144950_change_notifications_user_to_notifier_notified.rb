812e92f5b3f799a08f3b171e4253517dfaf8a26f
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-28 22:20:49 -0400

    fixes wording in old migration: notifier_id -> notifying_user_id, notified_id -> notified_user_id

--- db/migrate/20160812144950_change_notifications_user_to_notifier_notified.rb
+++ db/migrate/20160812144950_change_notifications_user_to_notifier_notified.rb
@@ -1,6 +1,6 @@
 class ChangeNotificationsUserToNotifierNotified < ActiveRecord::Migration
   def change
-    rename_column :notifications, :user_id, :notified_id
-    add_column :notifications, :notifier_id, :integer, null: false
+    rename_column :notifications, :user_id, :notified_user_id
+    add_column :notifications, :notifying_user_id, :integer, null: false
   end
 end
