4a989c57148367fe2a3f5cba3d96518ec8b4052a
Andrew Williams <andrew.williams.atx@gmail.com>
2016-09-02 16:39:30 -0400

    after_action :destroy calls #destroy_request

--- app/controllers/api/friendships_controller.rb
+++ app/controllers/api/friendships_controller.rb
@@ -1,5 +1,6 @@
 class Api::FriendshipsController < ApplicationController
   before_action :require_login
+  after_action :destroy_request, only: :destroy
 
   def destroy
     friendship = Friendship.find_by( user_id: params[:id], friend_id: current_user.id)
@@ -11,5 +12,15 @@ class Api::FriendshipsController < ApplicationController
     @user_id = params[:id]
     @response = 'unfriend'
     render 'api/friendships/show'
+  end
+
+  private
+
+  def destroy_request
+    request = FriendRequest.find_by(maker_id: current_user.id, receiver_id: params[:id])
+    request.destroy if request
+
+    request = FriendRequest.find_by(maker_id: params[:id], receiver_id: current_user.id)
+    request.destroy if request
   end
 end
