595b9d2d114cf075723ef7e40dd11c2f71062258
Andrew Williams <andrew.williams.atx@gmail.com>
2016-09-02 23:21:43 -0400

    adds after_destroy :destroy_request

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,4 +2,15 @@ class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend,
     class_name: "User"
+
+  after_destroy :destroy_request
+
+  private
+
+  def destroy_request
+    request = FriendRequest.find_by(maker_id: user_id, receiver_id: friend_id)
+    request.destroy if request
+    request = FriendRequest.find_by(receiver_id: user_id, maker_id: friend_id)
+    request.destroy if request
+  end
 end
