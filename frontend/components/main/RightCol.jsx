2d75ec1c5a3277fb96b0d847a3e3244edc10192a
Andrew Williams <andrew.williams.atx@gmail.com>
2016-09-04 15:52:01 -0400

    right-col adds showcase blocks

--- frontend/components/main/RightCol.jsx
+++ frontend/components/main/RightCol.jsx
@@ -5,7 +5,43 @@ var RightCol = React.createClass({
     return (
       <aside id='right-col'>
         <article className='right-col-item'>
-          lol
+          <a className='right-col-container group'
+            href='https://github.com/awilliams-atx'
+            id='github'
+            target='_blank'>
+            <div className='showcase-icon right-col-content'>
+              <i className="fa fa-github" aria-hidden="true"></i>
+            </div>
+            <div className='showcase-text right-col-content'>
+              GitHub
+            </div>
+          </a>
+        </article>
+        <article className='right-col-item'>
+          <a className='right-col-container group'
+            href='https://www.linkedin.com/in/awilliamsatx'
+            id='linked-in'
+            target='_blank'>
+            <div className='showcase-icon right-col-content'>
+              Linked
+            </div>
+            <div className='showcase-text right-col-content'>
+              <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+            </div>
+          </a>
+        </article>
+        <article className='right-col-item'>
+          <a className='right-col-container group'
+            href='http://www.andrewwilliams.io'
+            id='portfolio'
+            target='_blank'>
+            <div className='showcase-icon right-col-content'>
+              aW
+            </div>
+            <div className='showcase-text right-col-content'>
+              Portfolio
+            </div>
+          </a>
         </article>
       </aside>
     );
