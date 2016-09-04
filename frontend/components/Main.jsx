dfd712737932e734b9d98cfef87b957075a42dfb
Andrew Williams <andrew.williams.atx@gmail.com>
2016-09-04 18:56:04 -0400

    removes #getInitialState

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -6,9 +6,6 @@ var React = require('react'),
     ClientActions = require('../actions/client_actions');
 
 var Main = React.createClass({
-  getInitialState: function () {
-    return null;
-  },
   render: function () {
     return (
       <div id='main'>
