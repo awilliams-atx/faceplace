286a240ba6593eb456de3a5225795a5e44018d42
Andrew Williams <andrew.williams.atx@gmail.com>
2016-09-06 16:50:31 -0400

    images table adds paperclip columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160906204824) do
+ActiveRecord::Schema.define(version: 20160906205001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,9 +48,13 @@ ActiveRecord::Schema.define(version: 20160906204824) do
   end
 
   create_table "images", force: :cascade do |t|
-    t.integer  "imageable_id", null: false
+    t.integer  "imageable_id",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "notifications", force: :cascade do |t|
