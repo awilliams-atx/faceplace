6f6dda8bec2ab1dac0f42e21552c88a72e8d0baf
Andrew Williams <andrew.williams.atx@gmail.com>
2016-09-06 16:52:19 -0400

    adds Image model

---
+++ app/models/image.rb
@@ -0,0 +1,5 @@
+class Image < ActiveRecord::Base
+  # --------------------------------PAPERCLIP-------------------------------- #
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+end
