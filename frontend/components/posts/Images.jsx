c4bbfa11bd00219bf3d652c41f020cb06b4ce795
Andrew Williams <andrew.williams.atx@gmail.com>
2016-09-06 21:39:22 -0400

    PostForm images add tinter, options with X

--- frontend/components/posts/Images.jsx
+++ frontend/components/posts/Images.jsx
@@ -10,7 +10,13 @@ function renderImage(image, remove, idx) {
   return (
     <div className='upload-image-container' key={idx}>
       <img src={URL.createObjectURL(image)} />
-      <div className='tinter' />
+      <div className='image-options tinter-before'>
+        <i className="fa fa-times"
+          aria-hidden="true"
+          data-idx={idx}
+          onClick={remove}>
+        </i>
+      </div>
     </div>
   );
 }
